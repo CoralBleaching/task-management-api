a333bcff0984e8faef7a203f61284f7adb263fff
Renato Marques <megadeth.destroys.metallica@gmail.com>
2024-06-21 14:51:31 -0300

    feature/setting-up-migrations-first-step

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,28 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TaskModule } from './task/task.module';
-import { DatabaseModule } from './database/database.module';
-import { ConfigModule } from '@nestjs/config';
+// import { DatabaseModule } from './database/#database.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TaskModule, DatabaseModule],
+    TypeOrmModule.forRootAsync({
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        autoLoadEntities: true, // frisar
+        synchronize: false,
+      }),
+      inject: [ConfigService]}),
+    TaskModule, 
+    // DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
